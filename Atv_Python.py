fb65859979f370de133b05888b7b978400a9d1b8
Luiz-Henrique03 <cumiranga@gmail.com>
2022-09-04 11:55:52 -0300

    Update

--- Atv_Python.py
+++ Atv_Python.py
@@ -4,26 +4,54 @@ from imaplib import Time2Internaldate
 jogador = []
 posição = []
 nota = []
-Elenco = ()
 Time =[]
+Time_Titular = []
+
 
 def criar():
     i = 0
-    for i in range(3):
-        if i == 0:
-            jogador.insert(i,"Alisson")
-            posição.insert(i,"Goleiro")
-            nota.insert(i,8)
-            
-        elif i == 1:
-            jogador.insert(i,"Paquetá")
-            posição.insert(i,"Meio-campista")
-            nota.insert(i,9)
-            
-        elif i == 2:
-            jogador.insert(i,"Neymar")
-            posição.insert(i,"Atacante")
-            nota.insert(i,10)
+    jogador.clear()
+    posição.clear()
+    nota.clear()
+    Time.clear()
+    Elenco = ()
+    for i in range(23):
+        if (i < 3):
+            jogador.insert(i, input("Digite o nome do goleiro: "))
+            posição.insert(i, "Goleiro")
+            nota.insert(i, int(input("Digite a nota: ")))
+            while nota[i] < 0 or nota[i] > 10:
+
+                  print("Nota invalida, digite novamente")
+                  nota.insert(i, int(input("Digite a nota do goleiro: ")))
+        elif (i >= 3 and i < 7):
+            jogador.insert(i, input("Digite o nome do lateral: "))
+            posição.insert(i, "Lateral")
+            nota.insert(i, int(input("Digite a nota: ")))
+            while nota[i] < 0 or nota[i] > 10:
+                print("Nota invalida, digite novamente")
+                nota.insert(i, int(input("Digite a nota: ")))
+        elif (i >= 7 and i < 11):
+            jogador.insert(i, input("Digite o nome do Zagueiro: "))
+            posição.insert(i, "Zagueiro")
+            nota.insert(i, int(input("Digite a nota: ")))
+            while nota[i] < 0 or nota[i] > 10:
+                print("Nota invalida, digite novamente")
+                nota.insert(i, int(input("Digite a nota do Zagueiro: ")))
+        elif (i >= 11 and i < 17):
+            jogador.insert(i,input("Digite o nome do Meio_Campista: "))
+            posição.insert(i, "Meio-campista")
+            nota.insert(i, int(input("Digite a nota: ")))
+            while nota[i] < 0 or nota[i] > 10:
+                print("Nota invalida, digite novamente")
+                nota.insert(i, int(input("Digite a nota do meia: ")))
+        elif (i >= 17 and i < 23):
+            jogador.insert(i,input("Digite o nome do Atacante: "))
+            posição.insert(i, "Atacante")
+            nota.insert(i, int(input("Digite a nota: ")))
+            while nota[i] < 0 or nota[i] > 10:
+                print("Nota invalida, digite novamente")
+                nota.insert(i, int(input("Digite a nota do atacante: ")))
         Elenco = (jogador[i],posição[i],nota[i])
         Time.append(Elenco)
             
@@ -36,19 +64,17 @@ def Atualiza():
         if Nome_Atualizar == Time[i][0] and Posicao_Atualizar == Time[i][1]:
             del Time[i]
             
-            jogador.insert(i,"Antony")
-            posição.insert(i,"Goleiro")
-            nota.insert(i,"7")
+            jogador.insert(i,input("Nome Do novo Jogador; "))
+            posição.insert(i,input("Posição do novo Jogador: "))
+            nota.insert(i,int(input("Nota do novo jogador: ")))
             Elenco_Aux = (jogador[i],posição[i],nota[i])
             Time.append(Elenco_Aux)
-            
-            
             break
   
             
 def Excluir():
-    Nome_Excluir= input("Digite o nome a ser alterado: ")
-    Posicao_Excluir = input("Digite a posição do jogador a ser alterado: ")
+    Nome_Excluir= input("Digite o nome a ser Excluido: ")
+    Posicao_Excluir = input("Digite a posição do jogador a ser excluido: ")
     
     i = 0
     for i in range(len(jogador)):
@@ -56,9 +82,122 @@ def Excluir():
             del Time[i]
             break
         elif i == len(jogador):
-            print("Dados não conferem") 
-                
-            
-criar()
-Atualiza()
-print(Time)
+            print("Dados não conferem")
+
+def MontaTimeTitular():
+    maior_nota = [0]
+    j = 0
+
+    Goleiro_Notas = []
+    for j in range(3, 7):
+        Goleiro_Notas.append(Time[j][2])
+    Goleiro_Notas.sort(reverse=True)
+
+    Lateral_Notas = []
+    for j in range(3, 7):
+        Lateral_Notas.append(Time[j][2])
+    Lateral_Notas.sort(reverse=True)
+
+    Zagueiro_Notas = []
+    for j in range(7, 11):
+        Zagueiro_Notas.append(Time[j][2])
+    Zagueiro_Notas.sort(reverse=True)
+
+    Meia_Notas = []
+    for j in range(11, 17):
+        Meia_Notas.append(Time[j][2])
+    Meia_Notas.sort(reverse=True)
+
+    Atacantes_Notas = []
+    for j in range(17, len(Time)):
+        Atacantes_Notas.append(Time[j][2])
+    Atacantes_Notas.sort(reverse=True)
+
+    Time_Titular.clear()
+
+    i=0
+    for i in range(23):
+
+        if (i < 3):
+            while len(Time_Titular) < 1:
+                if len(Time_Titular) < 3:
+                    if Goleiro_Notas[0] == Time[i][2]:
+                        Time_Titular.append(Time[i][0])
+                    elif Lateral_Notas[1] == Time[i][2]:
+                        Time_Titular.append(Time[i][0])
+
+        elif (i >= 3 and i < 7):
+            if len(Time_Titular) < 3:
+                if Lateral_Notas[0] == Time[i][2]:
+                    Time_Titular.append(Time[i][0])
+                elif Lateral_Notas[1] == Time[i][2]:
+                    Time_Titular.append(Time[i][0])
+
+        if (i >= 7 and i < 11):
+            if len(Time_Titular) < 5:
+                if Zagueiro_Notas[0] == Time[i][2]:
+                    Time_Titular.append(Time[i][0])
+                elif Zagueiro_Notas[1] == Time[i][2]:
+                    Time_Titular.append(Time[i][0])
+
+        if (i >= 11 and i < 17):
+            if len(Time_Titular) < 8:
+                if Meia_Notas[0] == Time[i][2]:
+                    Time_Titular.append(Time[i][0])
+                elif Meia_Notas[1] == Time[i][2]:
+                    Time_Titular.append(Time[i][0])
+                elif Meia_Notas[2] == Time[i][2]:
+                    Time_Titular.append(Time[i][0])
+
+        if (i >= 17 and i < 23):
+            if len(Time_Titular) < 11:
+                if Atacantes_Notas[0] == Time[i][2]:
+                    Time_Titular.append(Time[i][0])
+                elif Atacantes_Notas[1] == Time[i][2]:
+                    Time_Titular.append(Time[i][0])
+                elif Atacantes_Notas[2] == Time[i][2]:
+                    Time_Titular.append(Time[i][0])
+    print(Time_Titular)
+
+
+def Lista_Convocados():
+    i = 0
+    for i in range(len(Time)):
+        print(Time[i][0]+ " " +Time[i][1])
+
+
+op = 0
+Convocou = 0
+
+while op != 6:
+    print("1 - Fazer a convocação")
+    print("2 - Aterar Jogador")
+    print("3 - Excluir jogador")
+    print("4 - Exibir Jogadores Convocados")
+    print("5 - Exibir time Titular")
+    print("6 - Sair")
+    op = int(input("Escolha uma opção: "))
+
+    if op == 1:
+            criar()
+
+
+    if op == 2:
+            Atualiza()
+
+
+
+    if op == 3:
+            Excluir()
+
+
+
+    if op == 4:
+        Lista_Convocados()
+
+
+
+    if op == 5:
+            MontaTimeTitular()
+
+
